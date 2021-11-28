433696940a7274c64feba877175ea5ef791f33cc
Nat Epstein <nathaniel.epstein@gmail.com>
2021-11-28 10:13:57 -0500

    added functions so that a readme could be generated

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,32 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
+  ![Github licence](http://img.shields.io/badge/license-${data.license}-blue.svg)
+  
+  ## Description 
+  ${data.description}
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  
+  ## Installation 
+  ${data.installation}
+  ## Usage 
+  ${data.usage}
+  ## License 
+  This project is license under ${data.license}
+  ## Contributing 
+  ${data.contributing}
+  ## Tests
+  ${data.tests}
+  ## Questions
+  If you have any questions about this projects, please contact me directly at ${data.contact}. You can view more of my projects at https://github.com/${data.github}.
 `;
 }
 
--- utils/index.js
+++ utils/index.js
@@ -1,66 +1,86 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const generateMarkdown = require('./generateMarkdown');
 // TODO: Create an array of questions for user input
-const questions = [
-    {
-        type: 'input',
-        message: 'What is your github username? (Do not include @ in your response',
-        name: 'github'
-    },
-    { 
-        type: 'input',
-        message: 'What is the name of your github repo?',
-        name: 'repo'
-    },
-    {
-        type: 'input',
-        message: 'What is the title of your project',
-        name: 'project'
-    },
-    {
-        type: 'input',
-        message: 'Write a description of your project.',
-        name: 'description'
-    },
-    {
-        type: 'input',
-        message: 'Describe the installation requirements and steps for setup.',
-        name: 'installation'
-    },
-    {
-        type: 'input',
-        message: 'Please provide instructions and examples of your project in use for the Usage section.',
-        name: 'usage'
-    },
-    {
-        type: 'input',
-        message:  'Give guidelines on how other developers can contribute to your project.',
-        name: 'contributing'
-    },
-    {
-        type: 'input',
-        message: 'If applicable, provide any tests written for your application and provide examples on how to run them.',
-        name: 'tests'
-    },
-    {
-        type: 'list',
-        message: "Choose a license for your project.",
-        choices: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'The Unlicense'],
-        name: 'license'
-    },
-    {
-        type: 'input',
-        message: 'Provide your contact info (email)',
-        name: 'contact'
-    }
-];
+const questions = () =>  {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            message: 'What is your github username? (Do not include @ in your response',
+            name: 'github'
+        },
+        {
+            type: 'input',
+            message: 'What is the title of your project',
+            name: 'title'
+        },
+        {
+            type: 'input',
+            message: 'Write a description of your project.',
+            name: 'description'
+        },
+        {
+            type: 'input',
+            message: 'Describe the installation requirements and steps for setup.',
+            name: 'installation'
+        },
+        {
+            type: 'input',
+            message: 'Please provide instructions and examples of your project in use for the Usage section.',
+            name: 'usage'
+        },
+        {
+            type: 'input',
+            message:  'Give guidelines on how other developers can contribute to your project.',
+            name: 'contributing'
+        },
+        {
+            type: 'input',
+            message: 'If applicable, provide any tests written for your application and provide examples on how to run them.',
+            name: 'tests'
+        },
+        {
+            type: 'list',
+            message: "Choose a license for your project.",
+            choices: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'The Unlicense'],
+            name: 'license'
+        },
+        {
+            type: 'input',
+            message: 'Provide your contact info (email)',
+            name: 'contact'
+        }
+    ]
+
+    )
+}
 
 // TODO: Create a function to write README file
-function writeToFile('ExampleREADME.md', data) {}
+const writeToFile = data => {
+    fs.writeFile('README.md', data, err => {
+        if (err) {
+            console.log(err);
+            return;
+        } else {
+            console.log("You've successfully created your README!");
+        }
+    }) 
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+const init = () => {
+    questions()
+    .then(userInput => {
+        return generateMarkdown(userInput);
+    })
+    .then(data => {
+        return writeToFile(data);
+    })
+    .catch(err => {
+        console.log(err);
+    })
+}
 
 // Function call to initialize app
 init();
