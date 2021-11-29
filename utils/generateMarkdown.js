073a01c10286589ee5721a1334697d89945faac6
Nat Epstein <nathaniel.epstein@gmail.com>
2021-11-29 09:34:41 -0500

    license update

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -13,7 +13,7 @@ function renderLicenseSection(license) {}
 
 function generateMarkdown(data) {
   return `# ${data.title}
-  ![Github licence](http://img.shields.io/badge/license-${data.license}-blue.svg)
+  ![Github license](http://img.shields.io/badge/license-${data.license}-blue.svg)
   
   ## Description 
   ${data.description}
